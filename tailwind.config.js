ca09dc92a8ceb7020d8430cc15423914c2de034d
suraj belbase <surajbelbase@suraj-mac.local>
2023-07-09 12:14:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  darkMode:'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
